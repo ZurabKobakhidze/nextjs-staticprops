d3e1a9a7d4e0a3e02612b4a8d2dd8b9721aa1b50
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-19 14:32:34 +0400

    99 chapter

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,16 +13,28 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.product.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
       products: data.products,
     },
-    revalidate: 10
+    revalidate: 10,
   };
 }
 
